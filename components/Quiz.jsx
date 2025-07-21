ffe6ffd6e666967895814338630733b35bf5f8ee
hao-zhang-05 <hao@mmtech.com.au>
2025-07-21 15:00:48 +1000

    v0.2 successfully showing information after answering all the questions

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizComplete from "../assets/quiz-complete.png";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const isQuizFinished = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers(prevUserAnswers => {
@@ -12,12 +14,24 @@ export default function Quiz() {
         });
     }
 
+    if (isQuizFinished) {
+        return (
+            <div id="summary">
+                <img src={quizComplete} alt="Quiz complete" />
+                <h2>Quiz Complete!</h2>
+            </div>
+        );
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                    {shuffledAnswers.map(answer => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
